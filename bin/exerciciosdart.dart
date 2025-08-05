4b9fdcca7d1c78fd039ae5f196a12ebb2b305d8d
leandrodasilvamarques <leandrodasilva01921@gmail.com>
2025-08-04 21:58:49 -0300

    classe banco, metodos deposito e recebeOperacao implementados

--- bin/exerciciosdart.dart
+++ bin/exerciciosdart.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
 void main() {
-getComando();
+  List<String> todoList = <String>[];
+  programa(todoList);
 }
 
-String getComando(){
- bool sair = false;
+String input(){
  List<String> listaOpcoes= <String>["1", "2", "3"]; 
 
  print("1- Adicionar\n2- Listar\n3- Sair\n");
@@ -12,8 +12,42 @@ String getComando(){
 
   if(entrada == null || !listaOpcoes.contains(entrada)){
     print("Entrada invalida\n");
-    getComando();
+    input();
   }
+
  return entrada;
- 
 }
+
+void adicionarNota(List<String> lista){
+  print("Digite uma nova nota:\n");
+  String nota = stdin.readLineSync()!;
+  lista.add(nota);
+  print("Nota adicionada!\n");
+}
+
+void listarNotas(List<String> lista){
+  lista.forEach(print);
+}
+
+void programa(List<String> lista){
+
+  while (true){
+    String opcao = input();
+
+    switch(opcao){
+      case "1":
+       adicionarNota(lista);
+       break;
+
+      case "2" :
+       listarNotas(lista);
+       break;
+
+      case "3":
+      print("Saindo!\n");
+       return;
+    }
+  } 
+  
+}
+
